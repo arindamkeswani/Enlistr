0a3948b960f92bd07d111fab45d793c14d694999
Arindam <arindamkeswani@gmail.com>
2021-11-09 00:57:23 +0530

    Querying data

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,45 @@
+const mysql = require('mysql');
+
+
+//connection pool
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
 
 
 //Routing
 
 //view users
-exports.view = (req,res)=>{
-    res.render('home'); 
+exports.view = (req, res) => {
+    // res.render('home');
+
+
+    //Connect to DB
+    pool.getConnection((err, connection) => {
+        if (err)
+            throw err; //not connected
+        console.log('Connected as ID (user controller View DB ): ', connection.threadId);
+
+
+        // use the connection
+        connection.query('SELECT * FROM user where status="active"', (err, rows) => {
+            //When done with the connection, release it
+            connection.release();
+
+            if(!err){
+                res.render('home', {rows});
+            }else{
+                console.log(err);
+            }
+
+            // console.log("The data from user table: \n", rows);
+        });
+
+    })
 };
---
+++ views/testdata.sql
@@ -0,0 +1,10 @@
+INSERT INTO `user` 
+(`id`, `first_name`,  `last_name`,      `latest_degree`, `major`, `cgpa` , `domain_skills` ,     `graduation_year`, `achievements`,      `email`,                `contact`,         `status`) VALUES
+(NULL, 'Amanda',      'Nunes'    ,      'B.Tech',        'CSE' ,  '6.8',   'Microsoft Office',   '2020',             '',                 'anunes@ufc.com',       '012345 678910',    'active'),
+(NULL, 'Khabib',      'Nurmagomedov',   'BBA',          'Finance', '7.8',   'Excel, Stock Trading', '2021', 'Batch topper',             'knurmagomedov@ufc.com', '012345 678910',     'active'),
+(NULL, 'Nate',        'Diaz ',           'BBA',          'Marketing', '7.6',   'Microsoft Excel', '2021', 'Medallist',   'ndiaz@ufc.com',         '012345 678910',        'active'),
+(NULL, 'Conor',       'McGregor ',  'B.Tech',        'CSE' ,  '8.8',   'MERN Stack',   '2020',             'SIH 2020 1st prize',  'cmcGregor@ufc.com',     '012345 678910',        'active'),
+(NULL, 'Cris',        'Cyborg ',    'B.Tech',        'ECE' ,  '8.6',   'Angular, Algorithms',   '2021',             'SIH 2020 2nd prize',  'ccyborg@ufc.com',       '012345 678910',         'active'),
+(NULL, 'Tecia',       'Torres ',  'B.Sc',        'Comp. Sci.' ,  '8.6',   'MERN Stack',   '2020',             '',    'ttorres@ufc.com',       '012345 678910',          'active'),
+(NULL, 'Ronda',       'Rousey ', 'B.Tech',        'CSE' ,  '8.8',   'Excel',   '2022',             'Class representative',     'rrousey@ufc.com',       '012345 678910',          'active'),
+(NULL, 'Holly',       'Holm ',    'B.Tech',        'Mechanical' ,  '8.1',   'MERN Stack, Data Structures and Algorithms',   '2020',             'Bridge building competition 1st prize',    'hholm@ufc.com',         '012345 678910',          'active')
