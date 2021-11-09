d1d05762a0f25803cd2ecedc3edbeafd65f51ec9
Arindam <arindamkeswani@gmail.com>
2021-11-10 01:05:35 +0530

    Manual Search

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -43,3 +43,31 @@ exports.view = (req, res) => {
 
     })
 };
+
+//Find user by searching
+exports.find = (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err)
+            throw err; //not connected
+        console.log('Connected as ID (user controller Search DB ): ', connection.threadId);
+
+        let searchTerm = req.body.search
+        
+        // use the connection
+        //using "?" to prevent SQL_injection
+        var searchShort = '%' + searchTerm + '%';
+        connection.query('SELECT * FROM user where status = "active" AND (domain_skills LIKE ? OR first_name LIKE ? OR last_name LIKE ? OR latest_degree LIKE ? OR major LIKE ? OR cgpa LIKE ?OR graduation_year LIKE ?OR achievements LIKE ?OR email LIKE ?OR contact LIKE ?)    ', [searchShort, searchShort, searchShort, searchShort,searchShort, searchShort, searchShort, searchShort,searchShort, searchShort], (err, rows) => {
+            //When done with the connection, release it
+            connection.release();
+
+            if(!err){
+                res.render('home', {rows});
+            }else{
+                console.log(err);
+            }
+
+            // console.log("The data from user table: \n", rows);
+        });
+
+    })
+}
---
+++ server/controllers/voiceSearchController.js
@@ -0,0 +1,48 @@
+let recordBtn = document.querySelector(".record-btn");
+let textArea = document.querySelector("textarea");
+let transcriptBtn = document.querySelector("#txt");
+var speechRecognition = window.webkitSpeechRecognition;
+var recognition = new speechRecognition();
+
+var spokenContent="";
+
+recognition.continuous = true;
+var isRecordingOn = false;
+
+recognition.onerror = function(){
+    alert("Failed to perform voice-to-text at thia time. Please try again in some time.")
+}
+
+recognition.onresult = function(event){
+    var current= event.resultIndex;
+
+    var transcript =  event.results[current][0].transcript;
+
+    spokenContent += transcript;
+    textArea.value = spokenContent;
+}
+recordBtn.addEventListener("click", function(){
+    if(isRecordingOn){ //turn recording off
+        isRecordingOn = false;
+        recordBtn.style.backgroundColor = "white";
+        recordBtn.style.color = "black";
+        // recordBtn.style.border = "none";
+        recognition.stop();
+        console.log("Content:", spokenContent);
+
+    }else{ //turn recording on
+        // console.log("Recording on");
+        isRecordingOn = true;
+        recordBtn.style.backgroundColor = "lightgrey";
+        recordBtn.style.color = "red";
+        // recordBtn.style.border = "2px solid grey";
+        
+        if(spokenContent.length){ //check if text is already present
+            spokenContent="";
+        }
+
+        recognition.start();
+        textArea.value = "";
+    }
+})
+
